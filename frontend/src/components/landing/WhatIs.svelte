c27f332d21d02fc5f8c03a23518b4e4625794866
Crusty <crwhite0530@gmail.com>
2026-06-08 12:49:54 -0400

    Typo fixes

--- frontend/src/components/landing/WhatIs.svelte
+++ frontend/src/components/landing/WhatIs.svelte
@@ -7,7 +7,7 @@
         <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-300 mx-auto w-full text-center">What is <span class="text-blue-400">Invertli</span>?</h2>
 
         <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-250 mx-auto w-full">
-            Invertli is a <span class="italic">Learning Management System</span> for teachers aiming to making the flipped classroom model more acessible.
+            Invertli is a <span class="italic">Learning Management System</span> for teachers aiming to making the flipped classroom modelc more acessible.
         </p>
         <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-250 mx-auto w-full">
             Invertli keeps students engaged while learning the content at home so you can spend classtime reinforcing knowledge.
--- frontend/src/components/landing/WhoAre.svelte
+++ frontend/src/components/landing/WhoAre.svelte
@@ -7,7 +7,7 @@
         <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-300 mx-auto w-full text-center">Who are <span class="text-blue-400">We</span>?</h2>
 
         <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-250 mx-auto w-full">
-            We are just two college kids that want to fill the gaps in the feild of public education and make tools more accesseble to teachers.
+            We are just two college kids that want to fill the gaps in the field of public education and make tools more accessible to teachers.
         </p>
 
     </div>
