b66022de0449c1ec3e434e97b0460d852aed3dab
Mikey E <31020711+hieyou1@users.noreply.github.com>
2026-06-09 03:25:17 +0000

    git add

---
+++ frontend/src/lib/components/landing/Hero.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from "./Header.svelte";
+	import SectionWrapper from "./SectionWrapper.svelte";
+</script>
+<SectionWrapper>
+    <Header />
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-300 mx-auto w-full text-center">Teach <span class="text-slate-600 line-through">Traditionally</span><br/>
+            with <span class="text-blue-400">Invertli</span>
+        </h2>
+
+        <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-300 mx-auto w-full">
+            <span class="text-blue-400">Invert</span> how your class learns with a research-backed flipped classroom platform
+        </p>
+        <button class="border border-blue-400 rounded-md p-2 text-2xl bg-blue-400 text-white hover:bg-blue-700 transition-all ease-in-out hover:cursor-pointer">Get Started</button>
+    </div>
+</SectionWrapper>
---
+++ frontend/src/lib/components/landing/WhatIs.svelte
@@ -0,0 +1,16 @@
+<script>
+	import SectionWrapper from "./SectionWrapper.svelte";
+</script>
+<SectionWrapper id="WhatIs">
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-300 mx-auto w-full text-center">What is <span class="text-blue-400">Invertli</span>?</h2>
+
+        <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-250 mx-auto w-full">
+            Invertli is a <span class="italic">Information Delivery System</span> for teachers aiming to making the flipped classroom model more acessible.
+        </p>
+        <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-250 mx-auto w-full">
+            Invertli keeps students engaged while learning the content at home so you can spend classtime reinforcing knowledge.
+        </p>
+
+    </div>
+</SectionWrapper>
